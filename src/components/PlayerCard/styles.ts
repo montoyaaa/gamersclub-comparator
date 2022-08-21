e3fa3f01147b6f2ed7ff17623ed26288c97346fc
montoyaaa <ricadomontoya2002@hotmail.com>
2022-08-20 21:23:52 -0300

    change white space to normal in title style

--- src/components/PlayerCard/styles.ts
+++ src/components/PlayerCard/styles.ts
@@ -134,5 +134,6 @@ export const Title = styled.span`
   line-height: 20px;
   font-size: 18px;
   text-transform: uppercase;
-  white-space: nowrap;
+  /* text-overflow: ellipsis; */
+  /* white-space: nowrap; */
 `;
