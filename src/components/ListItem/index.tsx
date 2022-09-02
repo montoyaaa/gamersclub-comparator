6c01f730501865c97fd0d90f5a09ce9af3f9504d
montoyaaa <ricardomontoya2002@hotmail.com>
2022-09-02 02:25:57 -0300

    create action section on list item

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,7 +1,18 @@
 import * as S from "./styles";
 
-const ListItem = ({ text = "Default" }: { text?: String }) => {
-  return <S.Wrapper>{text}</S.Wrapper>;
+const ListItem = ({
+  text,
+  actions,
+}: {
+  text: String;
+  actions?: JSX.Element[];
+}) => {
+  return (
+    <S.Wrapper>
+      {text}
+      <S.Actions>{actions}</S.Actions>
+    </S.Wrapper>
+  );
 };
 
 export default ListItem;
